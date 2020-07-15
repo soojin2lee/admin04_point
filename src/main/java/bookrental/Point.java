3059d27e845ca0c14821b5df8df5568085089c37
jy32 <65518795+jy32@users.noreply.github.com>
2020-07-15 22:53:49 +0900

    Add files via upload

--- src/main/java/bookrental/Point.java
+++ src/main/java/bookrental/Point.java
@@ -17,8 +17,8 @@ public class Point {
     private String status;
     private Date chgDate;
 
-    @PrePersist
-    public void onPrePersist(){
+    @PostPersist
+    public void onPostPersist(){
         if(this.getStatus().equals("saved")){
             Saved saved = new Saved();
             BeanUtils.copyProperties(this, saved);
